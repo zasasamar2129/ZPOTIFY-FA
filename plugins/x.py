241da8783203c1d63a22b2e77c9535c5e81a622f
zasasamar <92506162+zasasamar2129@users.noreply.github.com>
2024-09-01 12:08:58 +0330

    Update x.py

--- plugins/x.py
+++ plugins/x.py
@@ -22,8 +22,8 @@ class X:
     @staticmethod
     async def take_screenshot_of_tweet(event, tweet_url):
         tweet_message = await event.respond(
-            "Processing your request ...\nPlease wait while the screenshot is being generated.\nThis may take a few "
-            "moments.")
+            "در حال پردازش درخواست شما ...\nلطفا تا زمانی که اسکرین شات در حال تولید است صبر کنید.\nاین ممکن است چند ثانیه طول بکشد "
+            "لحظات.")
 
         settings = await TweetCapture.get_settings(event.sender_id)
         night_mode = settings['night_mode']
@@ -42,7 +42,7 @@ class X:
 
         except Exception as Err:
             await tweet_message.edit(
-                f"We apologize for the inconvenience.\nThe requested tweet could not be found. Reason: {str(Err)}")
+                f"بابت ناراحتی پیش آمده پوزش می طلبیم.\nتوییت درخواستی یافت نشد. دلیل: {str(Err)}")
             return None
 
     @staticmethod
@@ -51,7 +51,7 @@ class X:
         screenshot_path = await X.take_screenshot_of_tweet(event, tweet_url)
         has_media = await X.has_media(tweet_url)
 
-        screen_shot_message = await event.respond("Uploading the screenshot. Please stand by...")
+        screen_shot_message = await event.respond("در حال آپلود اسکرین شات لطفا منتظر باشید...")
 
         button = Button.inline("Download Media",
                                data=f"X/dl/{tweet_url.replace('https://x.com/', '')}"
@@ -60,7 +60,7 @@ class X:
             await client.send_file(
                 event.chat_id,
                 screenshot_path,
-                caption="Here is the requested tweet screenshot.",
+                caption="این تصویر توییت درخواستی است.",
                 buttons=button
             )
         except Exception as Err:
@@ -107,11 +107,11 @@ class X:
                             return True
                         return False
                     else:
-                        print(f"Error fetching URL: {normalized_link} (status code: {response.status})")
+                        print(f"خطا در واکشی URL: {normalized_link} (status code: {response.status})")
                         return False
 
         except Exception as e:
-            print(f"Error checking media in URL: {e}")
+            print(f"خطا در بررسی رسانه در URL: {e}")
             return False
 
     @staticmethod
@@ -143,12 +143,12 @@ class X:
 
         if media_url:
             try:
-                upload_message = await event.reply("Uploading Media ... Please hold on.")
+                upload_message = await event.reply("در حال آپلود رسانه ... لطفا صبر کنید.")
                 await client.send_file(event.chat_id, media_url,
-                                       caption="Thank you for using - @Spotify_YT_Downloader_Bot")
+                                       caption="با تشکر از شما برای استفاده - @z_smdbot")
                 await upload_message.delete()
             except Exception as e:
                 print(f"Error sending file: {e}")
-                await event.reply("Oops Invalid link or Media Is Not Available :(")
+                await event.reply("اوه پیوند نامعتبر یا رسانه در دسترس نیست :(")
         else:
-            await event.reply("Oops Invalid link or Media Is Not Available :(")
+            await event.reply("اوه پیوند نامعتبر یا رسانه در دسترس نیست :(")
