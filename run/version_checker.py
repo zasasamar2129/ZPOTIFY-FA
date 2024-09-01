d29facf27ceec88f3e0786712872dd3552981836
zasasamar <92506162+zasasamar2129@users.noreply.github.com>
2024-09-01 12:18:15 +0330

    Update version_checker.py

--- run/version_checker.py
+++ run/version_checker.py
@@ -4,7 +4,7 @@ from utils import db
 async def update_bot_version_user_season(event) -> bool:
     user_id = event.sender_id
     if not await db.check_username_in_database(user_id):
-        await event.respond("We Have Updated The Bot, Please start Over using the /start command.")
+        await event.respond("ما ربات را به‌روزرسانی کرده‌ایم، لطفاً استفاده از آن را دوباره شروع کنید /start فرمان.")
         await db.set_user_updated_flag(user_id, 0)
         return False
     await db.set_user_updated_flag(user_id, 1)
